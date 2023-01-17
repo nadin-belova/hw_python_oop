c14e0c36c01f0a7ebf1ba8579e622653323614e9
Надежда Белова <belovanadezhda1983@gmail.com>
2023-01-17 13:45:54 +0300

    Add dataclass to InfoMessage

--- homework.py
+++ homework.py
@@ -1,19 +1,14 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-
-    def __init__(self,
-                 training_type: str,
-                 duration: float,  # длительность тренировки
-                 distance: float,
-                 speed: float,
-                 calories: float,
-                 ) -> None:
-
-        self.training_type = training_type
-        self.duration = duration
-        self.distance = distance
-        self.speed = speed
-        self.calories = calories
+    training_type: str  # тип
+    duration: float  # длительность
+    distance: float  # дистанция
+    speed: float  # средняя скорость
+    calories: float  # потрачено ккал
 
     def get_message(self) -> str:
         return (f'Тип тренировки: {self.training_type}; '
